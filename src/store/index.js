a49c206e5d37f709d5c527cac04f9719e86c6956
Nekk123 <nemned123@gmail.com>
2021-12-28 09:27:42 +0100

    commit

---
+++ src/store/index.js
@@ -0,0 +1,40 @@
+import { createStore } from "vuex";
+import axios from "axios";
+
+export default createStore({
+  state: {
+    counter: 0,
+    colorCode: "blue",
+  },
+  mutations: {
+    increaseCounter(state, randomNumber) {
+      state.counter += randomNumber;
+    },
+    decreaseCounter(state, randomNumber) {
+      state.counter -= randomNumber;
+    },
+  },
+  actions: {
+    increaseCounter({ commit }) {
+      console.log("increaseCounter (aciton)");
+      axios(
+        "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new"
+      ).then((response) => {
+        commit("increaseCounter", response.data);
+      });
+    },
+    decreaseCounter({ commit }) {
+      axios(
+        "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new"
+      ).then((response) => {
+        commit("decreaseCounter", response.data);
+      });
+    },
+  },
+  getters: {
+    counterSquere(state) {
+      return state.counter * state.counter;
+    },
+  },
+  modules: {},
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="counter" :style="{ color: $store.state.colorCode }">
+      {{ $store.state.counter }}
+    </div>
+    <div class="counter-squered">
+      {{ $store.state.counter }}
+      <sup>2</sup> = {{ $store.getters.counterSquere }}
+    </div>
+    <div class="buttons">
+      <button @click="$store.dispatch('decreaseCounter')">-</button>
+      <button @click="$store.dispatch('increaseCounter')">+</button>
+    </div>
+    <div>
+      <input v-model="colorCode" placeholder="Enter color code" type="text" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style>
+div {
+  margin-top: 10px;
+}
+
+.counter {
+  font-size: 80px;
+}
+
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+  cursor: pointer;
+}
+</style>
